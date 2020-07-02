6c4636660b602ed355f7ea3c1b7deb7c943ce788
Matthew Charles Law <matthew.rottenegg@gmail.com>
2020-07-02 13:04:29 +0200

    Updatting init script

--- init_pi.sh
+++ init_pi.sh
@@ -1,5 +1,7 @@
+cd /
+git clone --single-branch --branch "master" https://github.com/matthewclaw/mjmOverseer.git
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt install -y nodejs
 sudo apt-get install -y npm
-git clone --single-branch --branch "master" https://github.com/matthewclaw/mjmOverseer.git
+cd mjmOverseer
 npm i
