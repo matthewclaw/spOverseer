23dfe14a77ab55ea65c198ea6e989801ebbb7314
Matthew Charles Law <matthew.rottenegg@gmail.com>
2020-07-02 12:52:53 +0200

    Adding init script

---
+++ init_pi.sh
@@ -0,0 +1,5 @@
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt install -y nodejs
+sudo apt-get install -y npm
+git clone --single-branch --branch "master" https://github.com/matthewclaw/mjmOverseer.git
+npm i
