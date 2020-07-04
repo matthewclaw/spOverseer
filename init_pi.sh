43f3c5a3d16c01e0848574e59fc2e42e52b861e8
Matthew Charles Law <matthew.rottenegg@gmail.com>
2020-07-04 16:50:04 +0200

    script fix

--- init_pi.sh
+++ init_pi.sh
@@ -2,8 +2,9 @@ echo "Cloning master"
 git clone --single-branch --branch "master" https://github.com/matthewclaw/mjmOverseer.git
 chmod ugo-wrx -R mjmOverseer
 cd mjmOverseer
+sudo apt-get update
 echo "Installing Node.js"
-sudo apt install -y nodejs
+sudo apt-get install -y nodejs
 echo "Installing npm"
 sudo apt-get install -y npm
 echo "Installing Node Modules"
