00d75a932de34eba5fef5bf4e85b2273178a5e80
Matthew Charles Law <matthew.rottenegg@gmail.com>
2020-07-05 12:06:17 +0200

    Updating script

--- init_pi.sh
+++ init_pi.sh
@@ -2,6 +2,8 @@ echo "Cloning master"
 git clone --single-branch --branch "master" https://github.com/matthewclaw/spOverseer.git
 chmod ugo+wrx -R mjmOverseer
 cd mjmOverseer
+mkdir "drop_locations"
+mkdir "Logs"
 sudo apt-get update
 echo "Installing Node.js"
 sudo apt-get install -y nodejs
